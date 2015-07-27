e853795bc2081f1a7bd6c8849dbed508dc8fa071
koreyba <koreybadenis@gmail.com>
2015-07-27 16:19:57 +0300

    Two files were added

---
+++ LottoSend.com/StringExtention.cs
@@ -0,0 +1,23 @@
+﻿namespace LottoSend.com
+{
+    public static class StringExtention
+    {
+        /// <summary>
+        /// Delets all spaces in a string
+        /// </summary>
+        /// <returns>String without spaces</returns>
+        public static string DeleteAllSpaces(this string str)
+        {
+            string stringWithoutSpaces = "";
+            for (int i = 0; i < str.Length; ++i)
+            {
+                if (str[i] != ' ')
+                {
+                    stringWithoutSpaces += str[i];
+                }
+            }
+
+            return stringWithoutSpaces;
+        }
+    }
+}
