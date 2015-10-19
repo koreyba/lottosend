14ee1f353c8e6441031a653b85df72a2ab731bf4
koreyba <koreybadenis@gmail.com>
2015-10-19 18:33:28 +0300

    daily

---
+++ LottoSend.com/BackEndObj/NewScannedImageObj.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace LottoSend.com.BackEndObj
+{
+    /// <summary>
+    /// Page Object of New Scanned Image (add) page
+    /// </summary>
+    class NewScannedImageObj : DriverCover
+    {
+        public NewScannedImageObj(IWebDriver driver) : base(driver)
+        {
+            if (!Driver.Url.Contains("scanned_images/new"))
+            {
+                throw new Exception("Sorry, it must be not New Scanned Image page, check it. The current URL is: " + Driver.Url + " ");
+            }
+
+            PageFactory.InitElements(Driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = "#scanned_image_lottery_id")]
+        private IWebElement _lottery;
+
+        [FindsBy(How = How.CssSelector, Using = "#scanned_image_kind")]
+        private IWebElement _kind;
+
+        [FindsBy(How = How.CssSelector, Using = "#scanned_image_local_officer_id")]
+        private IWebElement _localOfficer;
+
+        [FindsBy(How = How.CssSelector, Using = "#scanned_image_group_id")]
+        private IWebElement _group;
+
+        [FindsBy(How = How.CssSelector, Using = "#scanned_image_server_image")]
+        private IWebElement _chooseFile;
+
+        [FindsBy(How = How.CssSelector, Using = "#scanned_image_submit_action > input")]
+        private IWebElement _createScannedImageButton;
+
+        /// <summary>
+        /// Adds a new image to the specific lottery
+        /// </summary>
+        /// <param name="path">path to the picture</param>
+        /// <param name="lottery">lottery name</param>
+        public ScannedImagesObj AddNewImage_Single(string path, string lottery)
+        {
+            ChooseElementInSelect(lottery, _lottery, SelectBy.Text);
+
+            ChooseElementInSelect("Single Match", _kind, SelectBy.Text);
+
+            ChooseElementInSelect("3", _localOfficer, SelectBy.Index);
+            
+            _chooseFile.SendKeys(path);
+
+            _createScannedImageButton.Click();
+
+            WaitForPageLoading();
+            
+            return new ScannedImagesObj(Driver);
+        }
+
+        /// <summary>
+        /// Adds a new image to the specific lottery
+        /// </summary>
+        /// <param name="path">path to the picture</param>
+        /// <param name="lottery">lottery name</param>
+        /// <param name="group">name of group</param>
+        public ScannedImagesObj AddNewImage_Multiple(string path, string lottery, string group)
+        {
+            ChooseElementInSelect(lottery, _lottery, SelectBy.Text);
+
+            ChooseElementInSelect("Multiple Match", _kind, SelectBy.Text);
+
+            ChooseElementInSelect(group, _group, SelectBy.Text);
+            
+            _chooseFile.SendKeys(path);
+
+            _createScannedImageButton.Click();
+
+            WaitForPageLoading();
+
+            return new ScannedImagesObj(Driver);
+        }
+    }
+}
---
+++ LottoSend.com/TestCases/BackOffice/ManualMatchTests.cs
@@ -0,0 +1,105 @@
+﻿using System.IO;
+using LottoSend.com.BackEndObj;
+using LottoSend.com.Verifications;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace LottoSend.com.TestCases.BackOffice
+{
+    [TestFixture(typeof(ChromeDriver))]
+    [TestFixture(typeof(FirefoxDriver))]
+    //[TestFixture(typeof(InternetExplorerDriver))]
+    class ManualMatchTests<TWebDriver> where TWebDriver : IWebDriver, new() 
+    {
+        private IWebDriver _driver;
+        private DriverCover _driverCover;
+        private CommonActions _commonActions;
+        private BackOfficeVerifications _backOfficeVerifications;
+
+        /// <summary>
+        /// Adds a new scanned image for a group ticket
+        /// </summary>
+        /// <param name="lottery"></param>
+        /// <param name="group"></param>
+        [TestCase("El Gordo", "Winning Guarantee")]
+        [TestCase("Mega Millions", "Winning Guarantee")]
+        [TestCase("SuperLotto Plus", "Winning Guarantee")]
+        [TestCase("Powerball", "Winning Guarantee")]
+        [TestCase("EuroMillions", "Winning Guarantee")]
+        [TestCase("Euromiliony", "Random Selection")]
+        [TestCase("EuroJackpot", "Random Selection")]
+        [TestCase("EuroMillions", "Random Selection")]
+        [TestCase("EuroJackpot", "Random Selection")]
+        [TestCase("SuperLotto Plus", "Random Selection")]
+        [TestCase("Powerball", "Random Selection")]
+        [TestCase("Mega Millions", "Random Selection")]
+        [TestCase("El Gordo", "Random Selection")]
+        [TestCase("SuperEnalotto", "Random Selection")]
+        [TestCase("Euromiliony", "Systematic")]
+        [TestCase("Powerball", "Systematic")]
+        [TestCase("EuroMillions", "Additionals Guarantee")]
+        [TestCase("EuroJackpot", "Additionals Guarantee")]
+        [TestCase("Powerball", "New Winning")]
+        [TestCase("Powerball", "New Random")]
+        public void Add_New_Scanned_Image_Multiple(string lottery, string group)
+        {
+            _commonActions.SignIn_in_admin_panel();
+            _driverCover.NavigateToUrl(_driverCover.BaseAdminUrl + "admin/scanned_images/new");
+
+            NewScannedImageObj page = new NewScannedImageObj(_driver);
+            page.AddNewImage_Multiple(Path.Combine(Directory.GetCurrentDirectory(), @"Assets\scannedimage.jpg"), lottery, group);
+
+            ScannedImagesObj imagesPage = new ScannedImagesObj(_driver);
+
+            imagesPage.SwitchToMultipleTab();
+            Assert.IsTrue(imagesPage.GetTheFirstPictureName().Contains("scannedimage"), "Sorry but the scannedimage picture doesn't exist, maybe it wasn't uploaded. Please check it ");
+
+            imagesPage.FinishTheFirstMultiple();
+        }
+
+        /// <summary>
+        /// Adds a new scanned image for a single ticket
+        /// </summary>
+        /// <param name="lottery"></param>
+        [TestCase("El Gordo")]
+        [TestCase("Euromiliony")]
+        [TestCase("SuperLotto Plus")]
+        [TestCase("Mega Millions")]
+        [TestCase("Powerball")]
+        [TestCase("EuroMillions")]
+        [TestCase("SuperEnalotto")]
+        public void Add_New_Scanned_Image_Single(string lottery)
+        {
+            _commonActions.SignIn_in_admin_panel();
+            _driverCover.NavigateToUrl(_driverCover.BaseAdminUrl + "admin/scanned_images/new");
+
+            NewScannedImageObj page = new NewScannedImageObj(_driver);
+            page.AddNewImage_Single(Path.Combine(Directory.GetCurrentDirectory(), @"Assets\scannedimage.jpg"), lottery);
+            
+            ScannedImagesObj imagesPage = new ScannedImagesObj(_driver);
+
+            Assert.IsTrue(imagesPage.GetTheFirstPictureName().Contains("scannedimage"), "Sorry but the scannedimage picture doesn't exist, maybe it wasn't uploaded. Please check it ");
+
+            //now delete this picture
+            imagesPage.RemoveTheFirstPicture();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _driver.Dispose();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new TWebDriver();
+            _driverCover = new DriverCover(_driver);
+            _commonActions = new CommonActions(_driver);
+            _backOfficeVerifications = new BackOfficeVerifications(_driver);
+        }
+    }
+}
