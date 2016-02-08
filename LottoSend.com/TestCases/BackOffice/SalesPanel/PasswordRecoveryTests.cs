d3daebad571694e89ea68667d322918e929fe8d6
koreyba <koreybadenis@gmail.com>
2016-02-08 17:13:03 +0200

    Test for password recovery via sales panel added

---
+++ LottoSend.com/TestCases/BackOffice/SalesPanel/PasswordRecoveryTests.cs
@@ -0,0 +1,62 @@
+﻿using LottoSend.com.BackEndObj.SalesPanelPages;
+using LottoSend.com.Verifications;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace LottoSend.com.TestCases.BackOffice.SalesPanel
+{
+    [TestFixture(typeof(ChromeDriver))]
+    [Parallelizable(ParallelScope.Fixtures)]
+    public class PasswordRecoveryTests <TWebDriver> where TWebDriver : IWebDriver, new()
+    {
+        private IWebDriver _driver;
+        private DriverCover _driverCover;
+        private CommonActions _commonActions;
+        private WebUserVerifications _usersVerifications;
+
+        /// <summary>
+        /// Signs up a new user via the sales panel and changes its password. Then logs in on the front with this password
+        /// </summary>
+        [Test]
+        public void Recover_Password()
+        {
+            _commonActions.SignIn_in_admin_panel();
+
+            string email = "User_" + RandomGenerator.GenerateRandomString(6) + "@grr.la";
+            string password = "11111111";
+
+            _commonActions.Sign_Up_SalesPanel(email);
+
+            TabsObj tabs = new TabsObj(_driver);
+            tabs.GoToResetPasswordTab();
+
+            ResetPasswordTabObj recoveryPage = new ResetPasswordTabObj(_driver);
+            recoveryPage.ChangePassword(password);
+
+            _commonActions.Log_In_Front(email, password);
+            _usersVerifications.CheckIfSignedIn_Web();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                _driverCover.TakeScreenshot();
+            }
+            _driver.Dispose();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new TWebDriver();
+            _driverCover = new DriverCover(_driver);
+            _commonActions = new CommonActions(_driver);
+            _usersVerifications = new WebUserVerifications(_driver);
+        }
+    }
+}
