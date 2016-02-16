87c0d0a1ef76e573b1065e3c50beb7838b140d28
koreyba <koreybadenis@gmail.com>
2016-02-16 18:15:28 +0200

    daily

--- LottoSend.com/Program.cs
+++ LottoSend.com/Program.cs
@@ -1,4 +1,7 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using LottoSend.com.BackEndObj.ChargePanelPages;
+using LottoSend.com.FrontEndObj.Common;
+using LottoSend.com.TestCases;
+using OpenQA.Selenium.Chrome;
 
 
 namespace LottoSend.com
@@ -8,8 +11,20 @@ namespace LottoSend.com
         static void Main(string[] args)
         {
             ChromeDriver d = new ChromeDriver();
-            DriverCover driver = new DriverCover(d);
-            driver.TakeScreenshot();
+            CommonActions common = new CommonActions(d);
+            DriverCover _driverCover = new DriverCover(d);
+            common.SignIn_in_admin_panel();
+            _driverCover.NavigateToUrl(_driverCover.BaseAdminUrl + "admin/charge_panel_manager");
+            
+            for (int i = 0; i < 52; ++i)
+            {
+                ChargePanelObj panel = new ChargePanelObj(d);
+                ChargeFormObj form = panel.ChargeTheLastPayment();
+
+                form.MakeTransactionSucceed();
+                form.UpdateTransaction();
+                
+            }
         }
     }
 }
