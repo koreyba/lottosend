f8992fba98ae246bb8d9c862a9ffefe7a8777561
koreyba <koreybadenis@gmail.com>
2016-04-05 11:46:30 +0300

    env. chaged from test to stg

--- LottoSend.com/Program.cs
+++ LottoSend.com/Program.cs
@@ -1,6 +1,16 @@
-﻿using LottoSend.com.BackEndObj.ChargePanelPages;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using DisposableMail;
+using LottoSend.com.BackEndObj.ChargePanelPages;
+using LottoSend.com.FrontEndObj;
+using LottoSend.com.FrontEndObj.SignUp;
 using LottoSend.com.TestCases;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace LottoSend.com
 {
@@ -8,28 +18,23 @@ namespace LottoSend.com
     {
 
         static void Main(string[] args)
-        {
-            int a = 0;
-            ChromeDriver _driver = new ChromeDriver();
-            CommonActions _commonActions = new CommonActions(_driver);
-            DriverCover _driverCover = new DriverCover(_driver);
+        { 
+          
+
+            
 
 
-                //If pay with internal balance we need to log in with different user
-            _commonActions.Log_In_Front(_driverCover.Login, _driverCover.Password);
+            //_driverCover.NavigateToUrl(_driverCover.BaseAdminUrl + "admin/charge_panel_manager");
 
-            _commonActions.SignIn_in_admin_panel();
-            _driverCover.NavigateToUrl(_driverCover.BaseAdminUrl + "admin/charge_panel_manager");
+            //for (int i = 0; i < 52; ++i)
+            //{
+            //    ChargePanelObj panel = new ChargePanelObj(_driver);
+            //    ChargeFormObj form = panel.ChargeTheLastPayment();
 
-            for (int i = 0; i < 52; ++i)
-            {
-                ChargePanelObj panel = new ChargePanelObj(_driver);
-                ChargeFormObj form = panel.ChargeTheLastPayment();
+            //    form.MakeTransactionSucceed();
+            //    form.UpdateTransaction();
 
-                form.MakeTransactionSucceed();
-                form.UpdateTransaction();
-
-            }
+            // }
         }
     }
 }
