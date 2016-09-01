41c83b3f22795d1b74423a38ffa74321b0415851
koreyba <koreybadenis@gmail.com>
2016-09-01 15:48:06 +0300

    New project for new play page added. New page objects for new play page added

---
+++ PageObjects/FrontEndObj/Cart/NewOrderSummaryObj.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace TestFramework.FrontEndObj.Cart
+{
+    /// <summary>
+    /// Page object for a new "Order Summary" section on a new play page (site)
+    /// </summary>
+    public class NewOrderSummaryObj : DriverCover
+    {
+        public NewOrderSummaryObj(IWebDriver driver) : base(driver)
+        {
+            if (!Driver.FindElement(By.CssSelector(".always-display > td")).Displayed)
+            {
+                throw new Exception("Sorry but the Order Summary block is not displayed. Current URL is: " + Driver.Url + " ");
+            }
+
+            PageFactory.InitElements(Driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = ".summary_button")]
+        private IWebElement _proceedButton;
+
+        [FindsBy(How = How.CssSelector, Using = ".js--type-money")]
+        private IWebElement _totalPrice;
+
+        [FindsBy(How = How.XPath, Using = "..//span[@class='plus']")]
+        private IWebElement _addCouponButton;
+
+        [FindsBy(How = How.CssSelector, Using = "#reduction_code")]
+        private IWebElement _couponInputField;
+
+        [FindsBy(How = How.CssSelector, Using = ".btn.btn-info")]
+        private IWebElement _applyButton;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='coupon-container']//button[@class='close']")]
+        private IWebElement _closeButton;
+
+        [FindsBy(How = How.XPath, Using = "//*[@class='coupon']/td[2]")]
+        private IWebElement _couponDiscount;
+
+        /// <summary>
+        /// Returns total price 
+        /// </summary>
+        public double TotalPrice
+        {
+            get { return StringExtention.ParseDouble(_totalPrice.Text); }
+        }
+
+        /// <summary>
+        /// Returns the discount given an by applied coupon
+        /// </summary>
+        public double CouponDiscount
+        {
+            get { return StringExtention.ParseDouble(_couponInputField.Text); }
+        }
+
+        public void ApplyCoupon(string coupon)
+        {
+            _addCouponButton.Click();
+            WaitAjax();
+            _couponInputField.SendKeys(coupon);
+            _applyButton.Click();
+            WaitAjax();
+        }
+    }
+}
---
+++ PageObjects/FrontEndObj/GamePages/NewGroupGamePageObj.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace TestFramework.FrontEndObj.GamePages
+{
+    /// <summary>
+    /// Page object of a new play page on the site (web)
+    /// </summary>
+    public class NewGroupGamePageObj : DriverCover
+    {
+        public NewGroupGamePageObj(IWebDriver driver) : base(driver)
+        {
+            if (!Driver.FindElement(By.XPath("//*[@id='group']")).GetAttribute("class").Equals("active"))
+            {
+                throw new Exception("Sorry, it must be not new group game page ");
+            }
+
+            PageFactory.InitElements(Driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//button[contains(@class, 'btn-primary')]")]
+        private IList<IWebElement> _addToCartButtons;
+
+        [FindsBy(How = How.XPath, Using = "//a[@class='plus']")]
+        private IList<IWebElement> _plusButtons;
+
+        [FindsBy(How = How.XPath, Using = "//*[contains(@id, 'slide')]")]
+        private IList<IWebElement> _tickets;
+
+        /// <summary>
+        /// Adds shares to selected ticket (from 1 to 5)
+        /// </summary>
+        /// <param name="numberOfShares">How many shares to add</param>
+        public void AddShares(int numberOfShares)
+        {
+            _tickets[numberOfShares].Click();
+
+            IWebElement plus = GetFirstVisibleElementFromList(_plusButtons);
+            for (int i = 0; i < numberOfShares; ++i)
+            {
+                plus.Click();
+            }
+        }
+    }
+}
